14191c88a5f892b3a49f868e983d0d5f144dee34
shubh-soubhagya <soubhagyasrivastava240@gmail.com>
2025-11-20 00:36:16 +0530

    schema to store website content aswell

--- backend/src/models/Chat.js
+++ backend/src/models/Chat.js
@@ -1,15 +1,18 @@
 import mongoose from "mongoose";
 
+
 const chatSchema = new mongoose.Schema({
   userId: mongoose.Schema.Types.ObjectId,
   websiteUrl: String,
+  scrapedContent: String,   // <--- store here
   queryCount: { type: Number, default: 0 },
   messages: [
     {
-      role: String,  // 'user' or 'assistant'
+      role: String,
       content: String
     }
   ]
 });
+
 
 export default mongoose.model("Chat", chatSchema);
